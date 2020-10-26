0b2b7a53e1058f999b334625c12c015dbe0e2eae
meutizari <73374555+meutizari@users.noreply.github.com>
2020-10-26 10:46:24 +0700

    Add B1 files

---
+++ main/java/org/aplas/basicappx/MainActivity.java
@@ -0,0 +1,99 @@
+package org.aplas.basicappx;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+
+import java.text.DecimalFormat;
+
+public class MainActivity extends AppCompatActivity {
+    private Distance dist = new Distance();
+    private Weight weight = new Weight();
+    private Temperature temp = new Temperature();
+    private Button convertBtn;
+    private EditText inputTxt;
+    private EditText outputTxt;
+    private Spinner unitOri;
+    private Spinner unitConv;
+    private RadioGroup unitType;
+    private CheckBox roundBox;
+    private CheckBox formBox;
+    private ImageView imgView;
+    private AlertDialog startDialog;
+
+    public MainActivity(){
+
+    }
+
+    public MainActivity(Distance dist, Weight weight, Temperature temp){
+        this.dist = dist;
+        this.weight = weight;
+        this.temp = temp;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        convertBtn = (Button) findViewById(R.id.convertButton);
+        inputTxt = (EditText) findViewById(R.id.inputText);
+        outputTxt = (EditText) findViewById(R.id.outputText);
+        unitOri = (Spinner) findViewById(R.id.oriList);
+        unitConv = (Spinner) findViewById(R.id.convList);
+        unitType = (RadioGroup) findViewById(R.id.radioGroup);
+        roundBox = (CheckBox) findViewById(R.id.chkRounded);
+        formBox = (CheckBox) findViewById(R.id.chkFormula);
+        imgView = (ImageView) findViewById(R.id.img);
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        startDialog = new AlertDialog.Builder(MainActivity.this).create();
+        startDialog.setTitle("Application started");
+        startDialog.setMessage("This app can use to convert any units");
+        startDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+        startDialog.show();
+    }
+
+    protected double convertUnit(String type, String oriUnit, String convUnit, double value){
+        double convert = 0;
+        if(type.equalsIgnoreCase("Temperature")){
+            convert = temp.convert(oriUnit,convUnit, value);
+        } else if(type.equalsIgnoreCase("Distance")){
+            convert = dist.convert(oriUnit,convUnit, value);
+        } else if(type.equalsIgnoreCase("Weight")){
+            convert = weight.convert(oriUnit,convUnit, value);
+        }
+        return convert;
+    }
+
+    protected String strResult(double val, boolean rounded){
+        if(rounded){
+            val = Math.round(val*100.00)/100.00;
+        }
+        DecimalFormat s = new DecimalFormat("#.#####");
+        return s.format(val);
+    }
+}
